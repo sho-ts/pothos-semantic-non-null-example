e86e5aed795f6bbd43872bef718931c50f0cdbce
sho <sho.webfe@gmail.com>
2025-07-19 16:51:14 +0900

    update: スキーマのオーバーライド処理を追加

--- src/plugin-semantic-non-null/index.ts
+++ src/plugin-semantic-non-null/index.ts
@@ -1,5 +1,6 @@
 import SchemaBuilder, {
   BasePlugin,
+  type PothosOutputFieldConfig,
   type SchemaTypes,
 } from '@pothos/core';
 
@@ -7,6 +8,30 @@ const pluginName = 'semanticNonNull' as const;
 
 export default pluginName;
 
-export class SemanticNonNullPlugin<Types extends SchemaTypes> extends BasePlugin<Types> {}
+const directive = {
+  name: 'semanticNonNull',
+  args: {},
+};
+
+export class SemanticNonNullPlugin<Types extends SchemaTypes> extends BasePlugin<Types> {
+  override onOutputFieldConfig(
+    fieldConfig: PothosOutputFieldConfig<Types>
+  ): PothosOutputFieldConfig<Types> | null {
+    const option = fieldConfig.pothosOptions.semanticNonNull;
+
+    if (option) {
+      if (!Array.isArray(fieldConfig.extensions?.directives)) {
+        fieldConfig.extensions = {
+          ...fieldConfig.extensions,
+          directives: [directive],
+        };
+      } else {
+        fieldConfig.extensions?.directives.push(directive);
+      }
+    }
+
+    return fieldConfig;
+  }
+}
 
 SchemaBuilder.registerPlugin(pluginName, SemanticNonNullPlugin);
