e69413e9ac82ab3d740a607346258ee517e9c2af
sho <sho.webfe@gmail.com>
2025-07-19 16:52:04 +0900

    update: semanticNonNullの使用サンプル

--- src/builder.ts
+++ src/builder.ts
@@ -1,18 +1,30 @@
 import SchemaBuilder from '@pothos/core';
+import DirectivePlugin from '@pothos/plugin-directives';
+import SemanticNonNullPlugin from './plugin-semantic-non-null';
 
-export const builder = new SchemaBuilder<{
-  DefaultFieldNullability: false;
-  Context: {};
-}>({
-  defaultFieldNullability: false,
+export const builder = new SchemaBuilder({
+  plugins: [SemanticNonNullPlugin, DirectivePlugin],
+});
+
+const User = builder.objectRef<{
+  name: string | undefined
+}>('User');
+
+User.implement({
+  fields: (t) => ({
+    name: t.exposeString('name', {
+      semanticNonNull: true,
+    }),
+  }),
 });
 
 builder.queryType({
   fields: (t) => ({
-    healthCheck: t.int({
-      resolve: () => {
-        return 1;
-      },
+    user: t.field({
+      type: User,
+      resolve: () => ({
+        name: undefined,
+      }),
     }),
   }),
 });
